0649e0b7c3e916216a4072dd8c9e2fc38023cc36
Collins Kandie <collinskandie1@gmail.com>
2024-06-27 00:23:28 +0300

    paased all tests'

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Define the multiply variable with an equation whose result is 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Define the random variable to generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define the mod variable with an equation that calculates a remainder of 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Define the max variable to return 20 as the highest number in a set
+const max = Math.max(1, 2, 3, 20);
